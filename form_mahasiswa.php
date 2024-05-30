1aff5aeb0cac7360e611b4e501645b0410ee1034
rini afrizal sembiring <147572180+riniafrizalsembiringC1@users.noreply.github.com>
2024-05-30 12:04:17 +0700

    Create form_mahasiswa.php

---
+++ form_mahasiswa.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action= "save.php" method="post">
+    <input type="text" name="mhs[]" placeholder = "nama mahasiswa">
+    <input type="text" name="mhs[]" placeholder = "nama mahasiswa">
+    <input type="text" name="mhs[]" placeholder = "nama mahasiswa">
+    <input type="text" name="mhs[]" placeholder = "nama mahasiswa">
+    <input type="text" name="fakultas" placeholder = "nama Fakultas">
+    <input type="submit" value = "simpan">
+    </form>
+</body>
+</html>
